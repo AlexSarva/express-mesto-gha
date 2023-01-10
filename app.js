51a3a946b32a3f9cb376716687cafcc82c8651ff
alexsarva <alexsarva@gmail.com>
2023-01-10 13:56:08 +0300

    fix: fix issues

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const { PORT } = process.env;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,8 +31,6 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.patch('*', (req, res) => {
   res.status(404).send({ message: 'Not found' });
